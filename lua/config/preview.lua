a5af51f61a99ca84fbb1e1ab04ff614143bdbf13
steguiosaur <91378710+steguiosaur@users.noreply.github.com>
2023-05-02 15:42:45 +0800

    Change compiler in latexmk VimTex

--- lua/config/preview.lua
+++ lua/config/preview.lua
@@ -3,6 +3,16 @@ vim.g.tex_flavor = "latex"
 vim.g.vimtex_syntax_conceal_disable = true
 vim.g.vimtex_quickfix_mode = true
 vim.g.vimtex_view_method = "zathura"
+vim.g.vimtex_compiler_latexmk = {
+    executable = 'latexmk',
+    build_dir = 'output',
+    options = {
+        '-lualatex',
+        '-file-line-error',
+        '-synctex=1',
+        '-interaction=nonstopmode',
+    },
+}
 
 -- Markdown Preview
 vim.g.mkdp_theme = "dark"
